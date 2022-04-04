7e84390bc1329fb86117e6e7d4b772164c34167a
TVFKRCN <ercantevfik95@hotmail.com>
2022-04-04 17:27:08 +0300

    first commit

---
+++ 17b-project-NextJS- MeatupApp/pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// PSOT /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://AdminDeneme:LVf8IYiignKiW3Xj@cluster0.l2pro.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
